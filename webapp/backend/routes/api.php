4408e348edabb3b5d89d1bd718c4661878234e4d
Alejandro Téllez <alejandro.tellez@gmail.com>
2024-05-30 22:03:59 -0600

    Código de OpenClass S4

---
+++ webapp/backend/routes/api.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\MascotaController;
+use App\Http\Controllers\PropietarioController;
+use App\Http\Controllers\TipoController;
+use Illuminate\Support\Facades\Route;
+
+Route::group(['middleware'=>'api'], function($router) {
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+    Route::post('me', [AuthController::class, 'me']);
+} );
+
+Route::group(['middleware' => 'api', 'prefix' => 'v1'], function(){
+    // Siempre colocar la ruta en plural (para que Laravel encuentre el parámetro en singular en el controlador)
+    Route::apiResource('tipos', TipoController::class);
+    Route::apiResource('propietarios', PropietarioController::class);
+    Route::apiResource('mascotas', MascotaController::class);
+    Route::get('mascotas/catalogo/{propietario}', [MascotaController::class, 'catalogo']);
+});
+
+Route::get('login', function() {
+    return app()->abort(401);
+})->name('login');
--- webapp/dogtorpet/src/app/components/catalogo/catalogo.component.ts
+++ webapp/dogtorpet/src/app/components/catalogo/catalogo.component.ts
@@ -1,26 +1,63 @@
 import { Component, OnInit } from '@angular/core';
 import { Mascota } from '../../models/mascota';
-import { mascotasPrueba } from '../../util/datos-prueba';
 import { MascotaComponent } from '../mascota/mascota.component';
+import { Propietario } from '../../models/propietario';
+import { MascotaService } from '../../services/mascota.service';
+import { PropietarioService } from '../../services/propietario.service';
+import { OperatorFunction, Observable, debounceTime, distinctUntilChanged, filter, map } from 'rxjs';
+import { NgbTypeaheadModule, NgbTypeaheadSelectItemEvent } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-catalogo',
   standalone: true,
-  imports: [MascotaComponent],
+  imports: [MascotaComponent, NgbTypeaheadModule, FormsModule],
   templateUrl: './catalogo.component.html',
   styleUrl: './catalogo.component.css'
 })
 export class CatalogoComponent implements OnInit {
 
-  mascotas:Mascota[];
+  mascotas:Mascota[] = [];
+  propietarios: Propietario[] = [];
+  modelo!: Propietario;
 
-  constructor() {
-    // TODO: Reemplazarlo por una llamada al método lista() del servicio de mascotas
-    this.mascotas = mascotasPrueba;
-  }
+  formatter = (prop: Propietario) => `${prop.nombre} ${prop.apellidos} <${prop.email}>`;
+
+  constructor( private mascotaSvc:MascotaService, private propSvc:PropietarioService ) { }
 
   public ngOnInit(): void {
-    // TODO: Implementar la lógica para cargar todos los propietarios
+    const jsondoc = sessionStorage.getItem('propietario');
+    this.propSvc.lista().subscribe( props => this.propietarios = props );
+    if( jsondoc ) {
+      this.modelo = JSON.parse( jsondoc );
+      this.recargarTarjetas( this.modelo );
+    }
+  }
+
+  public actualizaMascotas( event:NgbTypeaheadSelectItemEvent ):void {
+    sessionStorage.setItem('propietario', JSON.stringify(event.item));
+    this.recargarTarjetas( event.item );
+  }
+
+  private recargarTarjetas( prop:Propietario ):void {
+    this.mascotaSvc.lista( prop.id ).subscribe(
+      arreglo => {
+        this.mascotas.length = 0;
+        this.mascotas.push(...arreglo);
+      }
+    );
+  }
+
+  search: OperatorFunction<string, readonly Propietario[]> = (text$: Observable<string>) =>
+		text$.pipe(
+			debounceTime(200),
+			distinctUntilChanged(),
+			filter((term) => term.length >= 2),
+			map((term) => this.propietarios.filter((prop) => new RegExp(term, 'mi').test(prop.email)).slice(0, 10)),
+		);
+
+  public eliminar( mascota:Mascota|null ): void {
+    // TODO: Implememtar lógica de eliminar un registro de mascotas
   }
 
 
